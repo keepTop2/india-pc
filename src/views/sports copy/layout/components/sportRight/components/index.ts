3cba724f14f9933d43503aa004e8c5956efd5226
suqin <suqin@whitesand.online>
2024-09-23 16:14:54 +0700

    删除sports备份代码

--- src/views/sports copy/layout/components/sportRight/components/index.ts
+++
@@ -1,6 +0,0 @@
-import SprotVideo from "./sprotVideo/index.vue";
-import PlayingMethod from "./playingMethod/index.vue";
-import { defineAsyncComponent } from "vue";
-// const SprotVideo = defineAsyncComponent(() => import("/@/views/sports/layout/components/sportRight/components/sprotVideo/index.vue"));
-// const PlayingMethod = defineAsyncComponent(() => import("/@/views/sports/layout/components/sportRight/components/playingMethod/index.vue"));
-export { SprotVideo, PlayingMethod };
--- src/views/sports copy/views/baseball/components/rollingCard/index.ts
+++
@@ -1,6 +0,0 @@
-import TeamInfoCard from "./components/teamInfoCard/teamInfoCard.vue";
-import MarketCard from "./components/marketCard/marketCard.vue";
-import EventItem from "./components/eventItem/eventItem.vue";
-import MarketColumn from "./components/marketColumn/marketColumn.vue";
-
-export { MarketCard, TeamInfoCard, EventItem, MarketColumn };
