947ddd70ac66f9209ec1b2df58dacd6afb721808
suqin <suqin@whitesand.online>
2024-10-09 14:54:25 +0700

    fix:修复路径错误

--- src/views/sports/tournamentViews/football/components/index.ts
+++ src/views/sports/tournamentViews/football/components/index.ts
@@ -2,5 +2,4 @@ import { defineAsyncComponent } from "vue";
 import { VirtualScrollVirtualList } from "/@/components/virtualScrollVirtualList/index";
 const FootballCard = defineAsyncComponent(() => import("/@/views/sports/tournamentViews/football/components/footballCard/footballCard.vue"));
 const SelectCard = defineAsyncComponent(() => import("/@/views/sports/components/selectCard/selectCard.vue"));
-const DateSelector = defineAsyncComponent(() => import("/@/views/sports/tournamentViews/football/components/dateSelector/dateSelector.vue"));
-export { FootballCard, SelectCard, VirtualScrollVirtualList, DateSelector };
+export { FootballCard, SelectCard, VirtualScrollVirtualList };
