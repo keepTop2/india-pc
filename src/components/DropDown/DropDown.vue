2d33801f770143000f9cdc090c5c6de3078bad87
suqin <suqin@whitesand.online>
2024-10-25 17:12:56 +0700

    提款ui编写 银行卡表单配置完善 接口数据调试（其余待完善）

---
+++ src/components/DropDown/DropDown.vue
@@ -0,0 +1,188 @@
+<template>
+	<div class="dropdown" ref="dropdownRef">
+		<slot name="toggle" :toggleDropdown="toggleDropdown">
+			<button @click="toggleDropdown" class="dropdown-toggle">
+				{{ selectedLabel }}
+			</button>
+		</slot>
+		<transition name="slide-fade">
+			<ul v-if="isOpen" class="dropdown-menu">
+				<div class="search_input">
+					<svg-icon class="search_icon" name="search" />
+					<input type="text" v-model="searchQuery" placeholder="请输入关键字" class="dropdown-search" />
+					<svg-icon v-show="searchQuery" class="search_clear" name="close" @click="searchQuery = ''" />
+				</div>
+				<div class="dropdown-list">
+					<li v-for="item in filteredOptions" :key="item[valueKey]" @click="selectOption(item)" class="dropdown-item">
+						<slot name="option" :option="item">
+							<span class="dropdown-item-value">{{ item[labelKey] }}</span>
+						</slot>
+					</li>
+				</div>
+			</ul>
+		</transition>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, onBeforeUnmount, defineEmits, watch, computed } from "vue";
+
+const props = defineProps<{
+	options: { [key: string]: string }[]; // 选项为任意键名的对象数组
+	defaultLabel?: string;
+	modelValue?: string;
+	searchValue?: string;
+	label?: string; // 显示值的键名
+	value?: string; // 绑定值的键名
+}>();
+
+const emit = defineEmits<{
+	(e: "update:modelValue", value: string): void;
+	(e: "update:searchValue", value: string): void;
+	(e: "toggle", isOpen: boolean): void;
+	(e: "change", item: { [key: string]: string }): void; // 新增选中事件
+}>();
+
+const labelKey = props.label || "label"; // 默认键名为 'label'
+const valueKey = props.value || "value"; // 默认键名为 'value'
+
+const selectedLabel = ref(props.modelValue ? props.options.find((option) => option[valueKey] === props.modelValue)?.[labelKey] : props.defaultLabel || "选择选项");
+const isOpen = ref(false);
+const dropdownRef = ref<HTMLElement | null>(null);
+const searchQuery = ref(props.searchValue || "");
+
+watch(searchQuery, (newVal) => {
+	emit("update:searchValue", newVal);
+});
+
+// 计算过滤后的选项
+const filteredOptions = computed(() => {
+	return props.options.filter((option) => option[labelKey].toLowerCase().includes(searchQuery.value.toLowerCase()));
+});
+
+const toggleDropdown = () => {
+	isOpen.value = !isOpen.value;
+	emit("toggle", isOpen.value);
+};
+
+const selectOption = (item: { [key: string]: string }) => {
+	selectedLabel.value = item[labelKey];
+	isOpen.value = false;
+	emit("update:modelValue", item[valueKey]);
+	emit("toggle", isOpen.value);
+	emit("change", item); // 触发选中事件
+};
+
+const handleClickOutside = (event: MouseEvent) => {
+	if (dropdownRef.value && !dropdownRef.value.contains(event.target as Node)) {
+		isOpen.value = false;
+		emit("toggle", isOpen.value);
+	}
+};
+
+onMounted(() => {
+	window.addEventListener("click", handleClickOutside);
+});
+
+onBeforeUnmount(() => {
+	window.removeEventListener("click", handleClickOutside);
+});
+</script>
+
+<style scoped lang="scss">
+.dropdown {
+	position: relative;
+	display: inline-block;
+}
+
+.dropdown-menu {
+	position: absolute;
+	top: 100%;
+	left: 0;
+	right: 0;
+	margin-top: 5px;
+	/* padding: 10px 0; */
+	border-radius: 8px;
+	background-color: var(--Bg3);
+	box-shadow: 0px 4px 10px 0px rgba(0, 0, 0, 0.2);
+	z-index: 1000;
+}
+
+.search_input {
+	width: 100%;
+	height: 100%;
+	display: flex;
+	align-items: center;
+	gap: 10px;
+	padding: 8px 10px;
+	border-bottom: 1px solid var(--Line_2);
+}
+
+input {
+	flex: 1;
+	margin: 0px;
+	padding: 0px;
+	border: 0px;
+	background-color: var(--Bg3);
+	color: var(--Text_s);
+	font-family: "PingFang SC";
+	font-size: 12px;
+	font-weight: 400;
+	&::placeholder {
+		color: var(--Text2_1);
+	}
+}
+
+.search_icon {
+	width: 14px;
+	height: 14px;
+}
+
+.search_clear {
+	width: 24px;
+	height: 24px;
+	cursor: pointer;
+}
+.dropdown-list {
+	height: 190px;
+	overflow-y: auto; /* 启用垂直滚动 */
+	scrollbar-width: none; /* 适用于 Firefox，隐藏滚动条 */
+	&::-webkit-scrollbar {
+		width: 0; /* 隐藏滚动条 */
+		height: 0;
+	}
+}
+
+.dropdown-item {
+	padding: 8px 12px;
+	cursor: pointer;
+	.dropdown-item-value {
+		color: var(--Text2_1);
+		font-family: "PingFang SC";
+		font-size: 12px;
+		font-weight: 400;
+	}
+}
+
+.dropdown-item:hover {
+	background-color: var(--Bg1);
+	.dropdown-item-value {
+		color: var(--Text_s);
+		font-family: "PingFang SC";
+		font-size: 12px;
+		font-weight: 400;
+	}
+}
+
+/* 过渡动画 */
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+	transition: opacity 0.2s ease, transform 0.2s ease;
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+	opacity: 0;
+	transform: translateY(-10px);
+}
+</style>
---
+++ src/views/wallet/withdrawal/components/BankCard/BankCard.vue
@@ -0,0 +1,226 @@
+<template>
+	<div class="form">
+		<template v-for="field in inputFields">
+			<div class="cell" v-if="!['bankName', 'userPhone'].includes(field.code) && isFieldVisible(field.code as keyof formParamsRootObject)">
+				<input v-model="formParams[field.model  as keyof formParamsRootObject]" :type="field.type" :placeholder="$t(`wallet['${field.placeholder}']`)" name="" id="" />
+			</div>
+			<!-- 银行名称 -->
+			<template v-if="field.code == 'bankName' && isFieldVisible(field.code as keyof formParamsRootObject)">
+				<DropDown
+					:options="withdrawWayConfig.bankList"
+					v-model="formParams['bankCode']"
+					v-model:searchValue="searchInput"
+					label="bankName"
+					value="bankCode"
+					@change="onChangeBankName"
+				>
+					<template #toggle="{ toggleDropdown }">
+						<div class="cell pointer" @click="toggleDropdown">
+							<input v-model="formParams['bankName']" :type="field.type" :placeholder="$t(`wallet['${field.placeholder}']`)" name="" id="" readonly />
+							<svg-icon class="icon" name="arrow_down" />
+						</div>
+					</template>
+					<template #option="{ option }">
+						<div class="custom-option">
+							<div class="icon">
+								<img :src="option.icon" alt="" />
+							</div>
+							<span class="label"> {{ option.bankName }}</span>
+						</div>
+					</template>
+				</DropDown>
+			</template>
+			<!-- 手机号 -->
+			<template v-if="field.code == 'userPhone' && isFieldVisible(field.code as keyof formParamsRootObject)">
+				<DropDown :options="areaCodeList" v-model="formParams['areaCode']" v-model:searchValue="searchInput" label="countryName" value="areaCode" @change="onChangeAreaCode">
+					<template #toggle="{ toggleDropdown }">
+						<div class="cell phone" :class="{ error: field.code === 'userPhone' && !isPhoneValid && formParams['userPhone'] }">
+							<div class="area_code" @click="toggleDropdown">
+								<span class="value">+{{ formParams["areaCode"] }}</span>
+								<svg-icon class="icon" name="arrow_down" />
+							</div>
+							<input v-model="formParams[field.model as keyof formParamsRootObject]" :type="field.type" :placeholder="$t(`wallet['${field.placeholder}']`)" name="" id="" />
+						</div>
+						<div v-if="field.code === 'userPhone' && !isPhoneValid && formParams['userPhone']" class="error_input_text">
+							{{ $t(`wallet['请输入位数字']`, { min: areaCodeInfo.minLength, max: areaCodeInfo.maxLength }) }}
+						</div>
+					</template>
+					<template #option="{ option }">
+						<div class="custom-option">
+							<div class="icon">
+								<img :src="option.icon" alt="" />
+							</div>
+							<span class="label">
+								<span>{{ option.countryCode }}</span>
+								<span>&nbsp;</span>
+								<span>{{ option.countryName }}</span>
+							</span>
+							<div class="value">+{{ option.areaCode }}</div>
+						</div>
+					</template>
+				</DropDown>
+			</template>
+		</template>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { computed, onMounted, reactive, ref } from "vue";
+import { CommonApi } from "/@/api/common";
+import Common from "/@/utils/common";
+
+const props = defineProps({
+	withdrawWayData: {
+		type: Object,
+		default: {},
+	},
+	withdrawWayConfig: {
+		type: Object,
+		default: {},
+	},
+});
+
+interface areaCodeListRootObject {
+	areaCode: string;
+	countryName: string;
+	countryCode: string;
+	icon: string;
+	maxLength: number;
+	minLength: number;
+}
+
+interface formParamsRootObject {
+	bankCard: string;
+	bankName: string;
+	bankCode: string;
+	userName: string;
+	surname: string;
+	provinceName: string;
+	cityName: string;
+	detailAddress: string;
+	userEmail: string;
+	areaCode: string;
+	userPhone: string;
+}
+
+const searchInput = ref("");
+const areaCodeList = ref([] as areaCodeListRootObject[]);
+const areaCodeInfo = ref({} as areaCodeListRootObject);
+const formParams = reactive(<formParamsRootObject>{
+	bankCard: "",
+	bankName: "",
+	bankCode: "",
+	userName: "",
+	surname: "",
+	provinceName: "",
+	cityName: "",
+	detailAddress: "",
+	userEmail: "",
+	areaCode: "",
+	userPhone: "",
+});
+
+// 输入字段的映射数组
+const inputFields = [
+	{ code: "bankCard", model: "bankCard", type: "text", placeholder: "请输入银行卡号" },
+	{ code: "bankName", model: "bankName", type: "text", placeholder: "请选择银行", readonly: true },
+	{ code: "bankCode", model: "bankCode", type: "text", placeholder: "请输入银行代码" },
+	{ code: "userName", model: "userName", type: "text", placeholder: "请输入取款人名" },
+	{ code: "surname", model: "surname", type: "text", placeholder: "请输入取款人姓" },
+	{ code: "provinceName", model: "provinceName", type: "text", placeholder: "请输入省" },
+	{ code: "cityName", model: "cityName", type: "text", placeholder: "请输入城市" },
+	{ code: "detailAddress", model: "detailAddress", type: "text", placeholder: "请输入详细地址" },
+	{ code: "userEmail", model: "userEmail", type: "text", placeholder: "请输入电子邮箱" },
+	{ code: "userPhone", model: "userPhone", type: "text", placeholder: "请输入手机号" },
+];
+
+onMounted(() => {
+	getAreaCodeDownBox();
+});
+
+// 手机号正则验证
+const isPhoneValid = computed(() => new RegExp(`^\\d{${areaCodeInfo.value.minLength},${areaCodeInfo.value.maxLength}}$`).test(formParams["userPhone"]));
+
+// 检查字段是否可见的函数
+const isFieldVisible = (code: keyof formParamsRootObject) => {
+	return (
+		props.withdrawWayConfig.collectInfoVOS &&
+		Array.isArray(props.withdrawWayConfig.collectInfoVOS) &&
+		props.withdrawWayConfig.collectInfoVOS.some((item) => item.filedCode === code)
+	);
+};
+
+// 获取区号下拉框数据的函数
+const getAreaCodeDownBox = () => {
+	CommonApi.getAreaCodeDownBox().then((res: any) => {
+		if (res.code == Common.ResCode.SUCCESS) {
+			areaCodeList.value = res.data;
+			areaCodeInfo.value = res.data[0];
+			formParams["areaCode"] = areaCodeInfo.value.areaCode;
+		}
+	});
+};
+
+const onChangeBankName = (e: any) => {
+	console.log("选择的银行信息", e);
+	formParams["bankName"] = e.bankName;
+};
+
+const onChangeAreaCode = (e: any) => {
+	console.log("选择的手机区号信息", e);
+};
+
+// 清空表单参数
+const clearParams = () => {
+	Object.keys(formParams).forEach((key) => {
+		formParams[key] = "";
+	});
+};
+
+// 暴露变量和方法
+defineExpose({
+	formParams,
+	isPhoneValid,
+	clearParams,
+});
+</script>
+
+<style scoped lang="scss">
+@import url("../formScss.scss");
+
+:deep(.custom-option) {
+	width: 100%;
+	height: 100%;
+	display: flex;
+	align-items: center;
+	gap: 10px;
+	.icon {
+		width: 14px;
+		height: 14px;
+		display: flex;
+		img {
+			width: 100%;
+			height: 100%;
+		}
+	}
+
+	.label,
+	.value {
+		color: var(--Text2_1);
+		font-family: "PingFang SC";
+		font-size: 12px;
+		font-weight: 400;
+	}
+
+	.label {
+		flex: 1;
+	}
+
+	&:hover {
+		.label,
+		.value {
+			color: var(--Text_s);
+		}
+	}
+}
+</style>
