619515d791416ea57f907d203aaa47a44929259e
liwei <liwei@whitesand.online>
2024-10-30 18:35:23 +0700

    fix:bug修复

--- src/views/activity/activityType/DAILY_COMPETITION/index.vue
+++ src/views/activity/activityType/DAILY_COMPETITION/index.vue
@@ -3,7 +3,7 @@
 		<div class="activityWrapper pb_20">
 			<div class="activityHeader">
 				{{ activityData.activityNameI18nCode || "每日竞赛" }}
-				<span class="closeIcon curp" @click="useModalStore().closeModal"><img src="../../components/image/close_icon.png" alt="" /></span>
+				<span class="closeIcon curp" @click="useModalStore().closeModal()"><img src="../../components/image/close_icon.png" alt="" /></span>
 			</div>
 			<div class="activityMain">
 				<div class="tabs">
