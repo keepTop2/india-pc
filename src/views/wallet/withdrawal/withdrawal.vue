e6102ceeb6241f727ec553f63cd28a8747b3c609
suqin <suqin@whitesand.online>
2024-10-31 11:53:56 +0700

    fix

--- src/views/wallet/withdrawal/withdrawal.vue
+++ src/views/wallet/withdrawal/withdrawal.vue
@@ -24,8 +24,8 @@
 			</div>
 		</Card>
 
-		<Card>
-			<div class="container mt_20">
+		<Card class="mt_20">
+			<div class="container">
 				<!-- 提款方式 -->
 				<div class="title">{{ $t(`wallet['提款方式']`) }}</div>
 				<div class="list">
