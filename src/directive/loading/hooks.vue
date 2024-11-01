f23f224d6c22906c291ab4ffa5a22c6bb13036ef
xiaofeng_c <xiaofeng_c@kingdee.com>
2024-11-01 12:49:51 +0800

    feat:调整loading样式

--- src/directive/loading/hooks.vue
+++ src/directive/loading/hooks.vue
@@ -1,9 +1,12 @@
 <template>
 	<div class="loading-mask">
 		<div class="typing-indicator">
+			<!-- <div class="typing-circle"></div>
 			<div class="typing-circle"></div>
-			<div class="typing-circle"></div>
-			<div class="typing-circle"></div>
+			<div class="typing-circle"></div> -->
+			<div class="dot1"></div>
+			<div class="dot2"></div>
+			<div class="dot3"></div>
 		</div>
 	</div>
 </template>
@@ -25,52 +28,93 @@ import {} from "vue";
 	left: 0;
 	z-index: 10000001;
 
+	// .typing-indicator {
+	// 	width: 60px;
+	// 	height: 30px;
+	// 	position: relative;
+	// 	z-index: 4;
+	// }
 	.typing-indicator {
-		width: 60px;
-		height: 30px;
-		position: relative;
-		z-index: 4;
-	}
+    display: flex;
 
-	.typing-circle {
-		width: 8px;
-		height: 8px;
-		position: absolute;
-		border-radius: 50%;
-		background-color: var(--Theme);
-		left: 15%;
-		transform-origin: 50%;
-		animation: typing-circle7124 0.5s alternate infinite ease;
-	}
+    .dot1,
+    .dot2,
+    .dot3 {
+       width: 8px;
+       height: 8px;
+       border-radius: 50%;
+       margin-left: 6px;
+     }
 
-	@keyframes typing-circle7124 {
-		0% {
-			top: 20px;
-			height: 5px;
-			border-radius: 50px 50px 25px 25px;
-			transform: scaleX(1.7);
-		}
+    .dot1 {
+       animation: jump 1.2s -0.4s linear infinite;
+       background: #ff0008;
+     }
 
-		40% {
-			height: 8px;
-			border-radius: 50%;
-			transform: scaleX(1);
-		}
+    .dot2 {
+       animation: jump 1.2s -0.2s linear infinite;
+       background: #ff0008;
+     }
 
-		100% {
-			top: 0%;
-		}
-	}
+    .dot3 {
+       animation: jump 1.2s linear infinite;
+       background: #ff0008;
+     }
 
-	.typing-circle:nth-child(2) {
-		left: 45%;
-		animation-delay: 0.2s;
-	}
+    @keyframes jump {
 
-	.typing-circle:nth-child(3) {
-		left: auto;
-		right: 15%;
-		animation-delay: 0.3s;
-	}
+      0%,
+      80%,
+      100% {
+        transform: scale(0.8);
+        opacity: 1.0;
+      }
+
+      40% {
+        transform: scale(2);
+        opacity: 0.8;
+      }
+    }
+  }
+	// .typing-circle {
+	// 	width: 8px;
+	// 	height: 8px;
+	// 	position: absolute;
+	// 	border-radius: 50%;
+	// 	background-color: var(--Theme);
+	// 	left: 15%;
+	// 	transform-origin: 50%;
+	// 	animation: typing-circle7124 0.5s alternate infinite ease;
+	// }
+
+	// @keyframes typing-circle7124 {
+	// 	0% {
+	// 		top: 20px;
+	// 		height: 5px;
+	// 		border-radius: 50px 50px 25px 25px;
+	// 		transform: scaleX(1.7);
+	// 	}
+
+	// 	40% {
+	// 		height: 8px;
+	// 		border-radius: 50%;
+	// 		transform: scaleX(1);
+	// 	}
+
+	// 	100% {
+	// 		top: 0%;
+	// 	}
+	// }
+
+	// .typing-circle:nth-child(2) {
+	// 	left: 45%;
+	// 	animation-delay: 0.2s;
+	// }
+
+	// .typing-circle:nth-child(3) {
+	// 	left: auto;
+	// 	right: 15%;
+	// 	animation-delay: 0.3s;
+	// }
 }
 </style>
--- src/directive/loading/index.vue
+++ src/directive/loading/index.vue
@@ -1,5 +1,12 @@
 <template>
-	<div class="loader_mask"><div class="loader"></div></div>
+	<div class="loader_mask">
+		<!-- <div class="loader"></div> -->
+		<div class="typing-indicator">
+			<div class="dot1"></div>
+			<div class="dot2"></div>
+			<div class="dot3"></div>
+		</div>
+	</div>
 </template>
 
 <script setup lang="ts"></script>
@@ -16,24 +23,64 @@
 	justify-content: center;
 	background-color: inherit;
 	transition: all 0.2s;
-}
+	.typing-indicator {
+    display: flex;
+    .dot1,
+    .dot2,
+    .dot3 {
+       width: 8px;
+       height: 8px;
+       border-radius: 50%;
+       margin-left: 6px;
+     }
 
-.loader {
-	width: 36px;
-	height: 36px;
-	border: 4px solid #ff284b;
-	border-left-color: transparent;
-	border-radius: 50%;
-	animation: spin89345 1s linear infinite;
-}
+    .dot1 {
+       animation: jump 1.2s -0.4s linear infinite;
+       background: #ff0008;
+     }
 
-@keyframes spin89345 {
-	0% {
-		transform: rotate(0deg);
+    .dot2 {
+       animation: jump 1.2s -0.2s linear infinite;
+       background: #ff0008;
+     }
+
+    .dot3 {
+       animation: jump 1.2s linear infinite;
+       background: #ff0008;
+     }
+
+    @keyframes jump {
+      0%,
+      80%,
+      100% {
+        transform: scale(0.8);
+        opacity: 1.0;
+      }
+
+      40% {
+        transform: scale(2);
+        opacity: 0.8;
+      }
+    }
 	}
-
-	100% {
-		transform: rotate(360deg);
-	}
 }
+
+// .loader {
+// 	width: 36px;
+// 	height: 36px;
+// 	border: 4px solid #ff284b;
+// 	border-left-color: transparent;
+// 	border-radius: 50%;
+// 	animation: spin89345 1s linear infinite;
+// }
+
+// @keyframes spin89345 {
+// 	0% {
+// 		transform: rotate(0deg);
+// 	}
+
+// 	100% {
+// 		transform: rotate(360deg);
+// 	}
+// }
 </style>
