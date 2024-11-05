48ed109b3b264a2b63c36afe2a43c19f2dcbc19b
skylar <mac@macs-Mac-mini.local>
2024-11-05 11:40:33 +0700

    暂存

---
+++ src/views/lottery/views/home/index.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="lottery">彩票</div>
+</template>
+
+<script setup lang="ts">
+// import useLottery from "/@/views/lottery/components/lotteryCard/Index";
+</script>
+
+<style lang="scss" scoped></style>
