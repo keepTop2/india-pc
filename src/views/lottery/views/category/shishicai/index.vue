01b64a462881677c024e6da35998713dd3cb8834
skylar <mac@macs-Mac-mini.local>
2024-11-07 15:25:35 +0700

    暂存

---
+++ src/views/lottery/views/category/shishicai/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="lottery-shishicai">
+		<!-- 头部 -->
+		<LotteryHeader />
+	</div>
+</template>
+
+<script setup lang="ts">
+import useLotteryHeader from "/@/views/lottery/components/LotteryHeader/Index";
+
+const { LotteryHeader, startTime, pauseTime, resetTime } = useLotteryHeader({ seconds: 68, immediate: false });
+</script>
+
+<style lang="scss" scoped>
+.lottery-shishicai {
+	width: 1308px;
+	margin: 0 auto;
+	margin-top: 24px;
+	background-color: var(--Bg1);
+	border-radius: 12px;
+}
+</style>
