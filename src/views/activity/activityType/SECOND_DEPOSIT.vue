523b313363348e74927cb20812bfb296bc3814b3
ahong <ahong121121@gmail.com>
2024-11-14 06:05:23 -0500

    修复活动 调用领取奖励接口bug

--- src/views/activity/activityType/SECOND_DEPOSIT.vue
+++ src/views/activity/activityType/SECOND_DEPOSIT.vue
@@ -119,12 +119,14 @@ const apply = async () => {
 			if (String(res.data.status).slice(0, 2) == "13") {
 				dialogInfo.value = res.data;
 				showCommonDialog.value = true;
-			} else {
-				activityApi.getActivityReward({ id: activityData.value.id }).then((res: any) => {
-					showToast(res.data.message);
-					activityStore.updateCurrentActivityData();
-				});
 			}
+			// else
+			// {
+			// 	activityApi.getActivityReward({ id: activityData.value.id }).then((res: any) => {
+			// 		showToast(res.data.message);
+			// 		activityStore.updateCurrentActivityData();
+			// 	});
+			// }
 		}
 	});
 };
